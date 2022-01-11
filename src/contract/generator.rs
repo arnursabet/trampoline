753d45f5f2e9f5b4262835aa0e30678cd6deea49
Tannr <tannr@hey.com>
2022-01-10 22:50:48 -0500

    Add query provider query skeleton

--- src/contract/generator.rs
+++ src/contract/generator.rs
@@ -1,6 +1,11 @@
+use std::rc::Rc;
 
+use ckb_types::core::cell::CellMeta;
 use ckb_types::core::{TransactionView, TransactionBuilder};
 use ckb_types::H256;
+use ckb_jsonrpc_types::{
+    Byte32, Script, OutPoint, Capacity
+};
 pub trait TransactionProvider {
     fn send_tx(&self) -> Option<H256>;
 
@@ -12,10 +17,35 @@ pub trait GeneratorMiddleware {
     fn pipe(&self, tx: TransactionView) -> TransactionView;
 }
 
-pub trait QueryProvider {
-    fn get_live_cell(&self) -> ();
 
-    fn get_live_cells(&self) -> ();
+// TODO: implement from for CellQueryAttribute on json_types and packed types
+#[derive(Debug, Clone)]
+pub enum CellQueryAttribute {
+    LockHash(Byte32),
+    LockScript(Script),
+    TypeScript(Script),
+    OutPoint(OutPoint),
+    Capacity(Capacity),
+}
+
+#[derive(Debug, Clone)]
+pub enum QueryStatement {
+    Single(CellQueryAttribute),
+    And(Rc<QueryStatement>, Rc<QueryStatement>),
+    Or(Rc<QueryStatement>, Rc<QueryStatement>),
+    // First statement is primary query, second statements filters from query matches
+    FilterFrom(Rc<QueryStatement>, Rc<QueryStatement>),
+}
+
+#[derive(Debug, Clone)]
+pub struct CellQuery {
+    query: QueryStatement,
+    limit: u64,
+}
+
+
+pub trait QueryProvider {
+    fn get_cells(&self, query: CellQuery) -> Vec<CellMeta>;
 }
 
 #[derive(Default)]
@@ -36,20 +66,29 @@ impl<'a, 'b> Generator<'a, 'b> {
         }
     }
 
-    pub fn set_pipeline(&mut self, pipes: Vec<&'a dyn GeneratorMiddleware>) {
+    pub fn pipeline(mut self, pipes: Vec<&'a dyn GeneratorMiddleware>) -> Self {
         self.middleware = pipes;
+        self
     }
 
-    pub fn set_chain_service(&mut self, chain_service: &'b dyn TransactionProvider) {
+    pub fn chain_service(mut self, chain_service: &'b dyn TransactionProvider) -> Self {
         self.chain_service = Some(chain_service);
+        self
     }
 
-    pub fn set_query_service(&mut self, query_service: &'b dyn QueryProvider) {
+    pub fn query_service(mut self, query_service: &'b dyn QueryProvider) -> Self {
         self.query_service = Some(query_service);
+        self
     }
 
     pub fn generate(&self) -> TransactionView {
-        self.middleware.iter().fold(self.tx.as_ref().unwrap().clone(), |tx, middleware| {
+        self.pipe(self.tx.as_ref().unwrap().clone())
+    }
+}
+
+impl GeneratorMiddleware for Generator<'_, '_> {
+    fn pipe(&self, tx: TransactionView) -> TransactionView {
+        self.middleware.iter().fold(tx, |tx, middleware| {
             middleware.pipe(tx)
         })
     }
